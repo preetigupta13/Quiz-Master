cd0d81920a78e8877b8afbd7e485ff96498897ff
Preeti Gupta <preetigupta5315@gmail.com>
2026-08-16 17:28:51 +0530

    Allow production frontend in CORS

--- backend/main.py
+++ backend/main.py
@@ -22,7 +22,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
     "http://localhost:5173",
-    "http://localhost:5174"
+    "http://localhost:5174",
+    "https://quiz-master-frontend-qlks.onrender.com"
 ]
     allow_credentials=True,
     allow_methods=["*"],
